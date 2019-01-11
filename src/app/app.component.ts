45ce15b9993027deb0b2a5b609a523cf13b2b12c
xtzaferis <xtzaferis@gmail.com>
2019-01-11 17:16:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  root = document.documentElement;
+  colors = ["#fff", "#000", "blue", "orange", "green"];
+
+  ngOnInit() {
+    // setInterval(() => {
+    //   this.root.style.setProperty('--color-primary-light', this.colors[this.getRandomInt(5)]);
+    // }, 2000);
+  }
+
+  private getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 }
